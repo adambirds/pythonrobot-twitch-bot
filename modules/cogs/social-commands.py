6bcaebc711111a182b89d338f2ced1a60abe1109
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2022-02-21 04:13:40 +0000

    refactor: Move social commands to seperate cog.

---
+++ modules/cogs/social-commands.py
@@ -0,0 +1,133 @@
+from twitchio.ext import commands
+
+
+class SocialCommandsCog(commands.Cog):
+    def __init__(self, bot: commands.Cog) -> None:
+        self.bot = bot
+
+    @commands.command()
+    async def github(self, ctx: commands.Context) -> None:
+        """
+        !github command
+        """
+        print(ctx.channel.name)
+        if self.bot.conf_options[ctx.channel.name]["SOCIALS"]["GITHUB"] != "":
+            await ctx.send(
+                f'Here is my GitHub link: {self.bot.conf_options[ctx.channel.name]["SOCIALS"]["GITHUB"]}'
+            )
+        else:
+            await ctx.send("I don't currently have a GitHub account.")
+
+    @commands.command()
+    async def discord(self, ctx: commands.Context) -> None:
+        """
+        !discord command
+        """
+        if self.bot.conf_options[ctx.channel.name]["SOCIALS"]["DISCORD"] != "":
+            await ctx.send(
+                f'Here is the link to my Discord server: {self.bot.conf_options[ctx.channel.name]["SOCIALS"]["DISCORD"]}'
+            )
+        else:
+            await ctx.send("I don't currently have a Discord server.")
+
+    @commands.command(aliases=["ig", "insta"])
+    async def instagram(self, ctx: commands.Context) -> None:
+        """
+        !instagram command
+        """
+        if self.bot.conf_options[ctx.channel.name]["SOCIALS"]["INSTAGRAM"] != "":
+            await ctx.send(
+                f'Here is the link to my Instagram page: {self.bot.conf_options[ctx.channel.name]["SOCIALS"]["INSTAGRAM"]}'
+            )
+        else:
+            await ctx.send("I don't currently have an Instagram page.")
+
+    @commands.command()
+    async def youtube(self, ctx: commands.Context) -> None:
+        """
+        !youtube command
+        """
+        if self.bot.conf_options[ctx.channel.name]["SOCIALS"]["YOUTUBE"] != "":
+            await ctx.send(
+                f'Here is the link to my YouTube channel: {self.bot.conf_options[ctx.channel.name]["SOCIALS"]["YOUTUBE"]}'
+            )
+        else:
+            await ctx.send("I don't currently have a YouTube channel.")
+
+    @commands.command()
+    async def twitter(self, ctx: commands.Context) -> None:
+        """
+        !twitter command
+        """
+        if self.bot.conf_options[ctx.channel.name]["SOCIALS"]["TWITTER"] != "":
+            await ctx.send(
+                f'Here is the link to my Twitter page: {self.bot.conf_options[ctx.channel.name]["SOCIALS"]["TWITTER"]}'
+            )
+        else:
+            await ctx.send("I don't currently have a Twitter page.")
+
+    @commands.command(aliases=["fb"])
+    async def facebook(self, ctx: commands.Context) -> None:
+        """
+        !facebook command
+        """
+        if self.bot.conf_options[ctx.channel.name]["SOCIALS"]["FACEBOOK"] != "":
+            await ctx.send(
+                f'Here is the link to my Facebook page: {self.bot.conf_options[ctx.channel.name]["SOCIALS"]["FACEBOOK"]}'
+            )
+        else:
+            await ctx.send("I don't currently have a Facebook page.")
+
+    @commands.command()
+    async def reddit(self, ctx: commands.Context) -> None:
+        """
+        !reddit command
+        """
+        if self.bot.conf_options[ctx.channel.name]["SOCIALS"]["REDDIT"] != "":
+            await ctx.send(
+                f'Here is the link to my Reddit: {self.bot.conf_options[ctx.channel.name]["SOCIALS"]["REDDIT"]}'
+            )
+        else:
+            await ctx.send("I don't currently have a Reddit.")
+
+    @commands.command()
+    async def tiktok(self, ctx: commands.Context) -> None:
+        """
+        !tiktok command
+        """
+        if self.bot.conf_options[ctx.channel.name]["SOCIALS"]["TIKTOK"] != "":
+            await ctx.send(
+                f'Here is the link to my tiktok: {self.bot.conf_options[ctx.channel.name]["SOCIALS"]["TIKTOK"]}'
+            )
+        else:
+            await ctx.send("I don't currently have tiktok.")
+
+    @commands.command()
+    async def website(self, ctx: commands.Context) -> None:
+        """
+        !website command
+        """
+        if self.bot.conf_options[ctx.channel.name]["SOCIALS"]["WEBSITE"] != "":
+            await ctx.send(
+                f'Here is the link to my website: {self.bot.conf_options[ctx.channel.name]["SOCIALS"]["WEBSITE"]}'
+            )
+        else:
+            await ctx.send("I don't currently have a website.")
+
+    @commands.command(aliases="links")
+    async def socials(self, ctx: commands.Context) -> None:
+        """
+        !socials command
+        """
+        social: str
+        social_text = ""
+        for social in self.bot.conf_options[ctx.channel.name]["SOCIALS"]:
+            if self.bot.conf_options[ctx.channel.name]["SOCIALS"][social] != "":
+                print(social.capitalize())
+                social_text += f'{social.capitalize()}: {self.bot.conf_options[ctx.channel.name]["SOCIALS"][social]} - '
+
+        await ctx.send(social_text.rstrip(" - "))
+
+
+def prepare(bot: commands.Bot) -> None:
+    bot.add_cog(SocialCommandsCog(bot))
